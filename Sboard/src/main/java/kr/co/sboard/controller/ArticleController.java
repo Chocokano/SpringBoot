89eadafef8011650d35d1e18ad6dbf40e0e339c9
lovelyroaflower <lovelyroaflower@gmail.com>
2023-01-20 13:24:52 +0900

    write,download 기능 추가 및 배포

--- Sboard/src/main/java/kr/co/sboard/controller/ArticleController.java
+++ Sboard/src/main/java/kr/co/sboard/controller/ArticleController.java
@@ -1,17 +1,25 @@
 package kr.co.sboard.controller;
 
+import java.io.IOException;
 import java.security.Principal;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import kr.co.sboard.service.ArticleService;
 import kr.co.sboard.vo.ArticleVO;
+import kr.co.sboard.vo.FileVO;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Controller
 public class ArticleController {
 
@@ -19,7 +27,24 @@ public class ArticleController {
 	private ArticleService service;
 	
 	@GetMapping("list")
-	public String list(Principal principal) {
+	public String list(Model model, String pg) {
+		
+		int currentPage = service.getCurrentPage(pg);
+		int start = service.getLimitStart(currentPage);
+		
+		int total = service.selectCountTotal();
+		int lastPageNum = service.getLastPageNum(total);
+		int pageStartNum = service.getPageStartNum(total, start);
+		int groups[] = service.getPageGroup(currentPage, lastPageNum);
+		
+		List<ArticleVO> articles = service.selectArticles(start);
+		
+		model.addAttribute("articles", articles);
+		model.addAttribute("currentPage", currentPage);
+		model.addAttribute("lastPageNum", lastPageNum);
+		model.addAttribute("pageStartNum", pageStartNum);
+		model.addAttribute("groups", groups);
+		
 		return "list";
 	}
 	
@@ -29,9 +54,29 @@ public class ArticleController {
 	}
 	
 	@GetMapping("view")
-	public String view() {
+	public String view(int no, Model model) {
+		ArticleVO article = service.selectArticle(no);
+		model.addAttribute("article", article);
 		return "view";
 	}
+	
+	@GetMapping("download")
+	public ResponseEntity<Resource> download(int fno) {
+		
+		FileVO vo = service.selectFile(fno);
+		service.updateFileDownload(fno);
+		
+		ResponseEntity<Resource> respEntity = null; 
+		
+		try {
+			respEntity = service.fileDownload(vo);
+		} catch (IOException e) {
+			log.info("error : "+e.getMessage());
+		}
+		
+		return respEntity;
+	}
+	
 	
 	@GetMapping("write")
 	public String write() {
